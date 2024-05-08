1e9b57f367d43ba53be690758cdbae2bb342b97d
Paulo henrique <henrique_reis@atlantico.com.br>
2024-05-08 08:05:01 -0300

    Adding aula10 - Web

---
+++ aula10-web/HTTP/http.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Olá Mundo!"))
+}
+
+func usuarios(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Carregar páginas de usuários!"))
+}
+
+func main() {
+
+	http.HandleFunc("/home", home)
+
+	http.HandleFunc("/usuarios", usuarios)
+
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
